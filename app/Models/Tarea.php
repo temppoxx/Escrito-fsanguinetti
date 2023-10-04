5eb60905f2b6494b80208ebaa9e1b0547d7ee703
Franco Sanguinetti <fsanguinetti05@gmail.com>
2023-10-04 14:34:03 -0300

    Corregido modelo

--- app/Models/Tarea.php
+++ app/Models/Tarea.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tarea extends Model
 {
     use HasFactory;
-    protected $table = tarea;
+    protected $table = 'tarea';
     protected $fillable = [
         'Titulo',
         'Contenido',
